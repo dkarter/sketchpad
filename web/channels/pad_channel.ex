1fc5c747752755a5a0730b7b6b0fcf99f31972d1
Dorian Karter <dkarter@gmail.com>
2016-08-31 13:34:01 -0400

    Add user list and presence
    Up to lunch break..

--- web/channels/pad_channel.ex
+++ web/channels/pad_channel.ex
@@ -1,5 +1,6 @@
 defmodule Sketchpad.PadChannel do
   use Sketchpad.Web, :channel
+  alias Sketchpad.Presence
 
   def join("pad:" <> pad_id, _params, socket) do
     send(self(), :after_join)
@@ -7,9 +8,36 @@ defmodule Sketchpad.PadChannel do
   end
 
   def handle_info(:after_join, socket) do
-    # if socket.assigns.user_id == "dorian" do
-    #   Process.exit(self(), :kill)
-    # end
+    %{user_id: user_id} = socket.assigns
+    #private communication to client
+
+    {:ok, _ref} = Presence.track(socket, user_id, %{device: "browser"})
+    push(socket, "presence_state", Presence.list(socket))
     {:noreply, socket}
+  end
+
+  def handle_in("stroke", data, socket) do
+    IO.puts "========= got stroke message"
+    IO.inspect data
+    # broadcast! = broadcast to everyone the new data
+    # broadcast_from! = broadcast to everyone but me the new data
+    
+    broadcast_from!(socket, "stroke", %{
+      user_id: socket.assigns.user_id,
+      stroke: data,
+    })
+    {:reply, :ok, socket}
+  end
+
+  def handle_in("clear", data, socket) do
+    IO.puts "========= got clear message"
+    IO.inspect data
+    # broadcast! = broadcast to everyone the new data
+    # broadcast_from! = broadcast to everyone but me the new data
+    
+    broadcast!(socket, "clear", %{
+      user_id: socket.assigns.user_id,
+    })
+    {:reply, :ok, socket}
   end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,16 +11,95 @@ let App = {
       params: { token }
     });
 
+
+
     this.sketchpadContainer = document.getElementById('sketchpad');
+    this.clearButton = document.getElementById('clear-button');
+    this.exportButton = document.getElementById('export-button');
+    this.usersContainer = document.getElementById('users');
+    this.presences = {};
+
     this.pad = new Sketchpad(this.sketchpadContainer, userId);
 
     socket.connect();
 
-    let padChannel = socket.channel("pad:lobby");
+    this.padChannel = socket.channel("pad:lobby");
 
-    padChannel.join()
+    this.padChannel.join()
       .receive("ok", resp => console.log("Joined!", resp))
       .receive("error", resp => console.log("join failed", resp) );
+
+    this.bind(this.padChannel);
+  },
+
+  bind(padChannel) {
+    this.pad.on('stroke', data => {
+      padChannel.push('stroke', data);
+        // .receive('ok', ...)
+        // .receive('error', ...)
+    });
+
+    this.clearButton.addEventListener('click', e => {
+      e.preventDefault();
+      padChannel.push('clear');
+    });
+
+    this.exportButton.addEventListener('click', e => {
+      console.log(this.pad);
+      window.open(this.pad.getImageURL());
+    });
+
+    padChannel.on('stroke', ({user_id, stroke}) => {
+      console.log('got stroke from user', user_id);
+      this.pad.putStroke(user_id, stroke, {color: '#000'});
+    });
+
+    padChannel.on('clear', ({user_id, stroke}) => {
+      console.log('got clear from user', user_id);
+      this.pad.clear();
+    });
+
+    padChannel.on('presence_state', state => {
+      this.presences = Presence.syncState(this.presences, state);
+      this.renderUsers();
+    });
+
+    padChannel.on('presence_diff', diff => {
+      this.presences = Presence.syncDiff(this.presences, diff,
+        this.onPresenceJoin.bind(this),
+        this.onPresenceJoin.bind(this)
+      );
+      this.renderUsers();
+    });
+  },
+
+  renderUsers() {
+    const listBy = (id, {metas: [first, ...rest]}) =>  {
+      first.id = id;
+      first.count = rest.length + 1;
+      return first;
+    };
+
+    const users = Presence.list(this.presences, listBy);
+    this.usersContainer.innerHTML = users.map(user => {
+      return `<br/>${sanitize(user.id)} (${user.count})`;
+    }).join("");
+  },
+
+  onPresenceJoin(id, currentPres, newPres) {
+    if (!currentPres) {
+      console.log(`${id} has joined for the first time`);
+    } else {
+      console.log(`${id} has joined from another device`);
+    }
+  },
+
+  onPresenceLeave(id, currentPres, leftPres) {
+    if(currentPres.metas.length === 0) {
+      console.log(`${id} has left entirely`);
+    } else {
+      console.log(`${id} has left from a device`);
+    }
   }
 };
 
